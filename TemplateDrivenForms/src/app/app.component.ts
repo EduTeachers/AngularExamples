cdf8b481a4eac9ba7baed118975e2c22afc6f416
Samuel Kodytek <samuelkodytek@Samuels-MacBook-Pro.local>
2022-05-09 11:23:40 +0200

    Template driven forms

---
+++ TemplateDrivenForms/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {User} from "./models/user.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TemplateDrivenForms';
+  username: string = "";
+  number: number = 0;
+  focus: boolean = false;
+  user: User = new User();
+
+  printUserInfo() {
+    console.log(this.user)
+  }
+}
